a6edfded75b0ec94c27a105345518f00882fde04
Morgane Ciot <morganeciot@gmail.com>
2020-01-09 16:15:17 -0500

    Renamed

--- mouse-hook/post_commit.py
+++ mouse-hook/post_commit.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import emoji
@@ -7,15 +8,34 @@ from commit import parse_commits
 from subprocess import check_output
 
 '''
+This script is run in the post-commit git hook. It checks for the environment variable MOUSE_WEIGHT
+to see if there is an existing mouse weight. If there is, it computes the weight delta given the
+existing mouse weight and the current commit. Otherwise it uses the following command to compute the
+historical weight:
+
 git log --author="author" --format="commit %H %at %s" --numstat
+
+The git config command is used to obtain the user's name:
+
+git config --list | grep '^user.name'
+
+If no username is set, the mouse will feed on every author's code contributions, rather
+promiscuously. After it computes the weight, it updates the environment variable with the current
+weight for next time. This is because the weight periodically returns to 0 if the mouse gets fed
+too much.
 '''
 
-def animate(strength):
-    animation_time = 0.3
-    strength_bar_total_size = 50
-    strength_bar_size = float(strength) / 100 * strength_bar_total_size
-    sleep_time = animation_time / strength_bar_size
+MAX_BAR_SIZE = 55
+ANIMATION_TIME = 0.3
 
+def get_bar_size():
+    _, columns = os.popen('stty size', 'r').read().split()
+    return min(int(columns) - 10, MAX_BAR_SIZE)
+
+def animate(strength):
+    strength_bar_total_size = get_bar_size()
+    strength_bar_size = float(strength) / 100 * strength_bar_total_size
+    sleep_time = ANIMATION_TIME / strength_bar_size
     for i in range(int(strength_bar_size)):
         time.sleep(sleep_time)
         strength = stylize(' ' * i, bg(state['color']))
@@ -23,7 +43,12 @@ def animate(strength):
         sys.stdout.write("\r" + bar)
         sys.stdout.flush()
 
-author = 'Morg'
+author = check_output([
+    'git',
+    'config',
+    '--get',
+    'user.name',
+])
 commit_history = check_output([
     'git',
     'log',
